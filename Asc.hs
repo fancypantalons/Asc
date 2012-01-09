36ca3bb184112d1233604f2aee4ae0eb76a8aae6
Brett Kosinski <brettk@b-ark.ca>
2012-01-08 22:27:52 -0700

    Factored out core state operations into separate module.
    Just a little fancy refactoring.  And while I was at it, fixed CORE by
    implementing a Show instance for Memory and making State derive Show.

--- Asc.hs
+++ Asc.hs
@@ -1,41 +1,16 @@
 module Asc where
 
 import Parser
+import State
 import Heap as Heap
 
 import System.IO
-import System.IO.Unsafe
 
 import Text.ParserCombinators.ReadP
-import Data.Array.IArray
-import Data.Array.IO
 import Data.List
 import Data.Char
+import Data.Array.IArray
 import System (getArgs)
-
-data Slot = ISlot Integer | DSlot Double | Empty
-instance Show Slot where
-  show (ISlot v) = show v
-  show (DSlot v) = show v
-  show Empty     = "_"
-
-type Memory = IOArray Integer Slot
-type RegisterSet = Array Integer Integer
-type InstrList = Array Integer Instr
-
-data State = State { 
-               running :: ! Bool,
-               pc      :: ! Integer,
-               sp      :: ! Integer,
-               tracing :: ! Bool,
-               display :: ! RegisterSet,
-               stack   :: ! Memory,
-               program :: ! InstrList,
-               heap    :: ! Heap.HeapInfo
-             } 
-
-instance Show State where
-  show state = ""
 
 --
 -- Split an array into a set of subarrays containing n or less elements
@@ -46,6 +21,11 @@ subgroups n []  = []
 subgroups n arr = group:(subgroups n rest)
   where group = take n arr
         rest  = drop n arr
+
+cmp :: (a -> a -> Bool) -> a -> a -> Integer
+cmp pred a b = case (pred a b) of
+                 True -> 1
+                 False -> 0
 
 --
 -- Given the specified program, extract all the labels and map them to their
@@ -79,23 +59,6 @@ resolveLabels p = map (\(_, i) -> translate i) p
         translate (CALL r (Named lbl)) = CALL r (resolve lbl)
         translate x                    = x
 
-newState :: Program -> IO State
-newState p = do
-  mem <- newListArray (0, 32767) [ Empty | i <- [0..32767] ]
-
-  return $ State {
-             display = array (0, 15) [ (i, 0) | i <- [0..15] ],
-             stack   = mem,
-             program = listArray (0, len - 1) listing,
-             pc      = 0,
-             sp      = 0,
-             heap    = Heap.newHeap 32768,
-             running = True,
-             tracing = False
-           }
-  where listing = resolveLabels p
-        len     = fromIntegral $ length listing
-
 --
 -- Various helper functions for the instruction implementation.
 -- 
@@ -104,85 +67,6 @@ translate :: State -> Addr -> Integer
 translate state (Absolute i)   = i
 translate state (Relative r i) = val + i
   where !val = regGet state r
-
-stackGet :: State -> Integer -> Slot
-stackGet state i = unsafePerformIO $ readArray (stack state) i
-
-stackSet :: State -> Integer -> Slot -> State
-stackSet state i slot = unsafePerformIO $ do
-  writeArray (stack state) i slot
-
-  return state
-
-stackElems :: State -> [Slot]
-stackElems state = unsafePerformIO $ getElems (stack state) 
-
-stackTop :: State -> Slot
-stackTop state = stackGet state $ (sp state - 1)
-
-stackTopI :: State -> Integer
-stackTopI state = case (stackTop state) of
-                    (ISlot val) -> val
-                    _           -> error "Expected integer but found something else"
-
-regGet :: State -> Integer -> Integer
-regGet state i = (display state) ! i
-
-regSet :: State -> Integer -> Integer -> State
-regSet state i val = state { display = (display state) // [(i, val)] }
-
-push :: State -> Slot -> State
-push state slot = newState { sp = sp newState + 1 }
-  where newState = stackSet state (sp state) slot
-
-pushi :: State -> Integer -> State
-pushi state val = push state (ISlot val)
-
-pushd :: State -> Double -> State
-pushd state val = push state (DSlot val)
-
-pop :: State -> (Slot, State)
-pop state 
-  | (sp state) == 0 = error "Tried to pop past top of stack"
-  | otherwise       = (slot, newState)
-  where newState = state { sp = sp state - 1 }
-        !slot     = stackGet newState (sp newState)
-
-popi :: State -> (Integer, State)
-popi state = 
-  case (pop state) of
-    ((ISlot val), state) -> (val, state)
-    _                    -> error "Expected integer slot but found something else"
-
-popd :: State -> (Double, State)
-popd state =
-  case (pop state) of
-    ((DSlot val), state) -> (val, state)
-    _                    -> error "Expected double slot but found something else"
-
-pop2 :: State -> (Slot, Slot, State)
-pop2 state = let (a, s1) = pop state 
-                 (b, s2) = pop s1
-             in (a, b, s2)
-
-combine :: (Slot -> Slot -> Slot) -> State -> State
-combine op state = push newState result
-  where (a, b, newState) = pop2 state
-        result           = op b a
-
-combinei :: (Integer -> Integer -> Integer) -> State -> State
-combinei op state = combine (\(ISlot a) (ISlot b) -> ISlot (op a b)) state
-
-combined :: (Double -> Double -> Double) -> State -> State
-combined op state = combine (\(DSlot a) (DSlot b) -> DSlot (op a b)) state
-
-combinedi :: (Double -> Double -> Integer) -> State -> State
-combinedi op state = combine (\(DSlot a) (DSlot b) -> ISlot (op a b)) state
-
-cmp :: (a -> a -> Bool) -> a -> a -> Integer
-cmp pred a b = case (pred a b) of
-                 True -> 1
-                 False -> 0
 
 --
 -- Instruction execution logic.
@@ -384,13 +268,13 @@ run state = do
     else return s2
 
 load :: [String] -> IO State
-load listing = newState $ parse listing
+load listing = newState $ resolveLabels $ parse listing
 
 loadFile :: String -> IO State
 loadFile f = do
   p <- parseFile f
 
-  newState p >>= return
+  newState (resolveLabels p) >>= return
 
 showHelp :: IO ()
 showHelp = do
---
+++ State.hs
@@ -0,0 +1,147 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module State
+(
+  Slot (ISlot, DSlot, Empty),
+  Memory,
+  RegisterSet,
+  InstrList,
+  State (running, pc, sp, tracing, display, stack, program, heap),
+  regGet,
+  regSet,
+  newState,
+  stackGet,
+  stackSet,
+  stackElems,
+  stackTop,
+  stackTopI,
+  push,
+  pushi,
+  pushd,
+  pop,
+  popi,
+  popd,
+  combine,
+  combinei,
+  combined,
+  combinedi
+)
+where
+
+import System.IO.Unsafe
+
+import Data.Array.IArray
+import Data.Array.IO
+
+import Parser
+import Heap as Heap
+
+data Slot = ISlot Integer | DSlot Double | Empty
+instance Show Slot where
+  show (ISlot v) = show v
+  show (DSlot v) = show v
+  show Empty     = "_"
+
+type Memory = IOArray Integer Slot
+instance Show Memory where
+  show mem = show $ unsafePerformIO $ getElems mem
+
+type RegisterSet = Array Integer Integer
+type InstrList = Array Integer Instr
+
+data State = State { 
+               running :: ! Bool,
+               pc      :: ! Integer,
+               sp      :: ! Integer,
+               tracing :: ! Bool,
+               display :: ! RegisterSet,
+               stack   :: ! Memory,
+               program :: ! InstrList,
+               heap    :: ! Heap.HeapInfo
+             } deriving Show
+
+newState :: [Instr] -> IO State
+newState listing = do
+  mem <- newListArray (0, 32767) [ Empty | i <- [0..32767] ]
+
+  return $ State {
+             display = array (0, 15) [ (i, 0) | i <- [0..15] ],
+             stack   = mem,
+             program = listArray (0, len - 1) listing,
+             pc      = 0,
+             sp      = 0,
+             heap    = Heap.newHeap 32768,
+             running = True,
+             tracing = False
+           }
+  where len     = fromIntegral $ length listing
+
+regGet :: State -> Integer -> Integer
+regGet state i = (display state) ! i
+
+regSet :: State -> Integer -> Integer -> State
+regSet state i val = state { display = (display state) // [(i, val)] }
+
+stackGet :: State -> Integer -> Slot
+stackGet state i = unsafePerformIO $ readArray (stack state) i
+
+stackSet :: State -> Integer -> Slot -> State
+stackSet state i slot = unsafePerformIO $ do
+  writeArray (stack state) i slot
+
+  return state
+
+stackElems :: State -> [Slot]
+stackElems state = unsafePerformIO $ getElems (stack state) 
+
+stackTop :: State -> Slot
+stackTop state = stackGet state $ (sp state - 1)
+
+stackTopI :: State -> Integer
+stackTopI state = case (stackTop state) of
+                    (ISlot val) -> val
+                    _           -> error "Expected integer but found something else"
+
+push :: State -> Slot -> State
+push state slot = newState { sp = sp newState + 1 }
+  where newState = stackSet state (sp state) slot
+
+pushi :: State -> Integer -> State
+pushi state val = push state (ISlot val)
+
+pushd :: State -> Double -> State
+pushd state val = push state (DSlot val)
+
+pop :: State -> (Slot, State)
+pop state 
+  | (sp state) == 0 = error "Tried to pop past top of stack"
+  | otherwise       = (slot, newState)
+  where newState = state { sp = sp state - 1 }
+        !slot     = stackGet newState (sp newState)
+
+popi :: State -> (Integer, State)
+popi state = 
+  case (pop state) of
+    ((ISlot val), state) -> (val, state)
+    _                    -> error "Expected integer slot but found something else"
+
+popd :: State -> (Double, State)
+popd state =
+  case (pop state) of
+    ((DSlot val), state) -> (val, state)
+    _                    -> error "Expected double slot but found something else"
+
+combine :: (Slot -> Slot -> Slot) -> State -> State
+combine op state = push newState result
+  where (a, s1)       = pop state 
+        (b, newState) = pop s1
+        result        = op b a
+
+combinei :: (Integer -> Integer -> Integer) -> State -> State
+combinei op state = combine (\(ISlot a) (ISlot b) -> ISlot (op a b)) state
+
+combined :: (Double -> Double -> Double) -> State -> State
+combined op state = combine (\(DSlot a) (DSlot b) -> DSlot (op a b)) state
+
+combinedi :: (Double -> Double -> Integer) -> State -> State
+combinedi op state = combine (\(DSlot a) (DSlot b) -> ISlot (op a b)) state
